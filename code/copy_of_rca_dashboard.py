8303c49ced42b35bd8caf30547dc927e22a27752
Nanigullipalli <126570117+Nanigullipalli@users.noreply.github.com>
2026-04-13 21:14:43 +0530

    Remove exclamation mark from pip install command

--- code/copy_of_rca_dashboard.py
+++ code/copy_of_rca_dashboard.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1-WjxS31PkdXsigy2z0MNwMF5zkfdlZKq
 """
 
-!pip install streamlit pyngrok plotly pandas scikit-learn -q
+pip install streamlit pyngrok plotly pandas scikit-learn -q
 
 # Commented out IPython magic to ensure Python compatibility.
 %%writefile app.py
